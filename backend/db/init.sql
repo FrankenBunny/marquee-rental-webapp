cc463a5fd55aafe2659cf7c35f25229abfe4082b
Maximilian Holm <c22mhm@cs.umu.se>
2025-05-11 09:31:11 +0200

    Updated syntax errors in sql files and added port to DB in compose.yaml file.

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,4 +1,11 @@
+/* Gamla koden
 CREATE TABLE marquee_user {
     INT PRIMARY KEY id,
     VARCHAR(50) UNIQUE username, 
 };
+*/
+
+CREATE TABLE marquee_user (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE
+);
--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,4 +1,10 @@
+/* Gamla koden
 INSERT INTO marquee_user (username) VALUES {
     ('Maxi'),
     ('Julia'),
 };
+*/ 
+INSERT INTO marquee_user (username)
+VALUES
+  ('Maximilian'),
+  ('Julia');
