6e63f1528d883904e26e03e7a1990c30be64f788
Julia Johansson <juliajsson@hotmail.com>
2025-07-25 14:19:38 +0200

    HOTFIX: Fixed static serving to node

--- api/src/middlewares/corsMiddleware.ts
+++ api/src/middlewares/corsMiddleware.ts
@@ -1,28 +1,30 @@
-import cors from 'cors';
-import config from '../config/config.js';
+import cors from "cors";
+import config from "../config/config.js";
 
 const corsOptions = () => {
-  if (config.nodeEnv === 'production') {
+  if (config.nodeEnv === "production") {
+    console.log(`CORS: http://${config.frontendOrigin}:${config.frontendPort}`);
     return {
       origin: [
         `http://${config.frontendOrigin}:${config.frontendPort}`,
         `https://${config.frontendOrigin}:${config.frontendPort}`,
+        `http://${config.frontendOrigin}`,
       ],
       credentials: true,
       optionsSuccessStatus: 200,
     };
-  } else if (config.nodeEnv === 'test') {
+  } else if (config.nodeEnv === "test") {
     return {
       origin: `http://localhost:${config.frontendPort}`,
       credentials: true,
       optionsSuccessStatus: 200,
     };
-  } 
-  return{
-      origin: `http://localhost:${config.frontendPort}`,
-      credentials: true,
-      optionsSuccessStatus: 200,
-    };
+  }
+  return {
+    origin: `http://localhost:${config.frontendPort}`,
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
 };
 
 export const corsMiddleware = cors(corsOptions());
